825302b0db84f599b6447839917e27e50740cb83
dandidcee <dandi12cahyaman12@gmail.com>
2026-07-09 02:38:36 +0700

    fix: unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { MdMenu } from 'react-icons/md';
 import Sidebar from './components/layout/Sidebar';
